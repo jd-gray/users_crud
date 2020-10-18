c8a9fcb3426c654c1231b91edabafaa545797918
Jared Gray <jared@jaredgray.us>
2020-10-18 11:34:45 -0700

    Add format and uniqueness validations to user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email, :phone, :status
+  validates_presence_of :name, :phone, :status
+  validates :email, 
+            presence: true, 
+            format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email invalid"  }, 
+            uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,5 +22,30 @@ RSpec.describe User, type: :model do
       subject.phone = nil
       expect(subject).to_not be_valid
     end
+
+    describe "Email" do
+      it "is valid with valid format" do
+        subject.email = "good@email.com"
+        expect(subject).to be_valid
+      end
+
+      it "is NOT valid with invalid format" do
+        subject.email = "bademail"
+        expect(subject).to_not be_valid
+        expect(subject.errors.messages[:email]).to eq ["Email invalid"]
+      end
+
+      it "is NOT valid with duplicate email" do
+        subject.save!
+        duplicate_user = described_class.new(name: "Jared Gray", email: "jaredgray.dev@gmail.com", phone: "714-767-6304")
+        expect(duplicate_user).to_not be_valid
+      end
+
+      it "is NOT valid with case insensitive duplicate email" do
+        subject.save!
+        duplicate_user = described_class.new(name: "Jared Gray", email: "JarEdgRay.dEv@Gmail.com", phone: "714-767-6304")
+        expect(duplicate_user).to_not be_valid
+      end
+    end
   end
 end
