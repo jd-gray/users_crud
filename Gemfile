a4458de291c1a43806c40f2988aad04653875d68
Jared Gray <jared@jaredgray.us>
2020-10-18 11:03:33 -0700

    Write some basic user validation tests and pair with guard to automatically run tests
    Add faker to gemfile for use later

--- Gemfile
+++ Gemfile
@@ -11,9 +11,12 @@ gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'faker', '~> 1.6', '>= 1.6.3'
 
 group :development, :test do
   gem 'rspec-rails', '~> 4.0', '>= 4.0.1'
+  gem 'guard', '~> 2.16', '>= 2.16.2'
+  gem 'guard-rspec', '~> 4.7', '>= 4.7.3'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    subject { described_class.new(name: "Jared Gray", email: "jaredgray.dev@gmail.com", phone: "714-767-6304", status: 0) }
+
+    it "is valid with required attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is NOT valid without name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is NOT valid without email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is NOT valid without phone" do
+      subject.phone = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
