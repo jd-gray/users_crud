9e764a95188d01bfb45b369533daaed706b0e6f8
Jared Gray <jared@jaredgray.us>
2020-10-20 20:39:27 -0700

    Put together crud functionality in api controller
    Fix date formatting js method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,48 @@ module Api
 
         render json: users
       end
+
+      def show
+        user = User.find_by(id: params[:id])
+
+        render json: user
+      end
+      
+      def create
+        user = User.new(user_params)
+        
+        if user.save
+          render json: user
+        else
+          render json: { error: user.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        user = User.find_by(id: params[:id])
+        
+        if user.update(user_params  )
+          render json: user
+        else
+          render json: { error: user.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        user = User.find_by(id: params[:id])
+
+        if user.destroy
+          head :no_content
+        else
+          render json: { error: user.errors }, status: 422
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email, :phone, :title, :status)
+      end
     end
   end
 end
